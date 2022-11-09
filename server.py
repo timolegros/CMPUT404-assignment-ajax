1ca2f3783e93fc290ece52072bf5d19af1d20fa1
Timothee Legros <timothee@ualberta.ca>
2022-11-09 14:58:04 -0500

    save

--- server.py
+++ server.py
@@ -74,27 +74,33 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return app.send_static_file("index.html")
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    myWorld.set(entity, request.json)
+    print(">>>>>>>>>>>>>>>>>>>>> MyWorld:", myWorld)
+    return entity
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    if request.method == 'GET':
+        return json.dumps(myWorld.world())
+    else:
+        return None
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return json.dumps(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return json.dumps({'status': 'Success'})
 
 if __name__ == "__main__":
     app.run()
